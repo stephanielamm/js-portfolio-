0e87b411e54d68757b8c3fd0ce0509f2f273034b
stephanielamm <slamm5@live.unc.edu>
2016-02-25 13:58:19 -0500

    initial commit

---
+++ js/style.js
@@ -0,0 +1,56 @@
+$(document).ready(function() { init() })
+
+//function init() {
+  //Tabletop.init( { key: '16CvigNSkrGPKT8KH7Te89mspSwsqm4LK_FF2q-wfBZc',
+                   //callback: portfolio,
+                   //simpleSheet: true } )
+//};
+
+//function portfolio(data, tabletop) {
+	//var clip_data = data;
+//	_.each(data, function(e, idx, list){
+	//	var compiled = _.template($('script.template-portfolio').html());
+	//	$('#portfolio-list').append(compiled({ 'clip': e }));
+	//	console.log(e.title);
+	//});
+//};
+
+$('.home').on('click', function(){
+	$('.portfolio-expand').removeClass('active');
+	$('.resume-expand').removeClass('active');
+  $('.contact-expand').removeClass('active');
+	$('.about-expand').removeClass('active');
+  $('.home-expand').addClass('active');
+});
+
+$('.about').on('click', function(){
+  $('.home-expand').removeClass('active');
+	$('.portfolio-expand').removeClass('active');
+	$('.resume-expand').removeClass('active');
+  $('.contact-expand').removeClass('active');
+	$('.about-expand').addClass('active');
+});
+
+$('.resume').on('click', function(){
+	$('.portfolio-expand').removeClass('active');
+  $('.home-expand').removeClass('active');
+	$('.about-expand').removeClass('active');
+  $('.contact-expand').removeClass('active');
+	$('.resume-expand').addClass('active');
+});
+
+$('.portfolio').on('click', function(){
+	$('.resume-expand').removeClass('active');
+  $('.home-expand').removeClass('active');
+	$('.about-expand').removeClass('active');
+  $('.contact-expand').removeClass('active');
+	$('.portfolio-expand').addClass('active');
+});
+
+$('.contact').on('click', function(){
+	$('.resume-expand').removeClass('active');
+  $('.home-expand').removeClass('active');
+	$('.about-expand').removeClass('active');
+	$('.portfolio-expand').removeClass('active');
+  $('.contact-expand').addClass('active');
+});
